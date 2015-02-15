90b26729305775eb4452ca71ded42faf4317d561
jtmancilla <jt.mancilla@gmail.com>
2015-02-14 21:13:28 -0600

    Tarea3

--- alumnos/jtmancilla/stdin.py
+++ alumnos/jtmancilla/stdin.py
@@ -3,40 +3,27 @@
 import re
 import sys
 
-# while True:
-#   linea = sys.stdin.readline()
+while True:
+  linea = sys.stdin.readline()
 
-#   if not linea:
-#     break
-#   # Hacemos algo con la línea
-#   #sys.stdout.write(linea)
-#   #sys.stdout.flush()
+  if not linea:
+    break
 
-
-
-#n = int(sys.argv[1]) # Leemos un entero como argumento (opcional)
-# data = ""
-# while 1:
-#     try:
-#         line = sys.stdin.readline()
-#     except KeyboardInterrupt:
-#         break
-
-#     if not line:
-#         break
-#     data += line
-#     #print line.upper()
-# print data
-
-
-num=0
-for line in sys.stdin:
-    regexp = re.compile("-?[0-9]+")
-    numeros=[int(i) for i in regexp.findall(line)]
-
-    for j in range(0,len(numeros)):
-      num = num + numeros[j]    
-    print num
-    num=0
-#sys.stdout.write(linea)
+  linea = re.sub("minutes","m",linea,flags=re.IGNORECASE)
+  linea = re.sub("minute","m",linea,flags=re.IGNORECASE)
+  linea = re.sub("min","m",linea,flags=re.IGNORECASE)
+  linea = re.sub("mi","m",linea,flags=re.IGNORECASE)
+  linea = re.sub("seconds","s",linea,flags=re.IGNORECASE)
+  linea = re.sub("second","s",linea,flags=re.IGNORECASE)
+  linea = re.sub("sec","s",linea,flags=re.IGNORECASE)
+  linea = re.sub("hours","h",linea,flags=re.IGNORECASE)
+  linea = re.sub("hour","h",linea,flags=re.IGNORECASE)
+  linea = re.sub("~", "", linea)
+  linea = re.sub(r"[^msh0-9]", "", linea)
+  # mas eficiente utiliza replace.
+  #linea = linea.replace('minutes','m')
+  #linea = linea.replace('minute','m')
+  print linea
+  #Hacemos algo con la linea
+  #sys.stdout.write(linea)
   #sys.stdout.flush()
