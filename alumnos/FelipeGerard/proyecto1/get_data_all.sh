7389dd1eb0640903b18cb8bc6be8bef9eb1feca4
Felipe Gerard <felipegerard@gmail.com>
2015-03-01 16:25:10 -0600

    obs_totales, top_5, top_5_anio, casi terminado rachas_por_estado

--- alumnos/FelipeGerard/proyecto1/get_data_all.sh
+++ alumnos/FelipeGerard/proyecto1/get_data_all.sh
@@ -1,5 +1,5 @@
 #! /bin/zsh
-# call 1: time sed 1,100p data_urls | ./get_data_all.sh # renglones 1 a 100
+# call 1: time sed -n 1,100p data_urls | ./get_data_all.sh # renglones 1 a 100
 # call 2: time sed 1,100d data_urls | ./get_data_all.sh # renglones a partir del 101
 
 
@@ -8,5 +8,7 @@ sed 's/.*\///' \
   "echo {} \
   | ./get_data.R \
   | awk ' {if(NR==1){print}} !/Date...Time/{print}' \
-  | sed -Ee 's/^\"//' -Ee 's/\"$//' -Ee 's/\" \"/|/g' \
-  | tr '|' '\t' > datos/{.}.txt"
+  | sed -Ee 's/^\"//' -Ee 's/\"$//' -Ee 's/\" \"/|/g' -e 's/Date...Time/Date_Time/' \
+  | tr '|' '\t' \
+  | sed -E 's/([0-9]+[^0-9]+)//' \
+  > datos/{.}.txt"
---
+++ alumnos/FelipeGerard/proyecto1/racha_por_estado.R
@@ -0,0 +1,43 @@
+#! /usr/bin/env Rscript
+require(dplyr, quietly = TRUE, warn.conflicts = FALSE)
+f <- file('stdin')
+open(f)
+
+df <- read.table(f, header = F, sep = "\t", skipNul = T, stringsAsFactors = F)
+# df <- read.table('~/big-data/alumnos/FelipeGerard/proyecto1/prueba',header = F,
+#                  sep = "\t",skipNul = T, stringsAsFactors = F)
+names(df) <- c('state', 'date')
+df$date <- as.Date(df$date)
+df <- filter(df, nchar(as.character(date)) & !is.na(date)) %>%
+  unique
+
+df$dif <- 1
+df$racha <- 1
+df$inicio <- as.Date('0000-01-01')
+df$fin <- as.Date('0000-01-01')
+inicio <- df$date[1]
+fin <- inicio
+racha = 1
+for(i in 2:nrow(df)){
+  df$dif[i] <- df$date[i] - df$date[i-1]
+  if(is.na(df$dif[i])) print(df[i,])
+  if(i %% 1000 == 0) print(i)
+  if(df$dif[i] == 1){
+    racha <- racha + 1
+    fin <- fin + 1
+  }else{
+    df$racha[i-1] <- racha
+    df$inicio[i-1] <- inicio
+    df$fin[i-1] <- fin
+    inicio <- df$date[i]
+    fin <- inicio
+    racha <- 1
+  }
+}
+
+df <- df %>% arrange(state, desc(racha))
+idx <- tapply(1:nrow(df), df$state, function(df) head(df,1)) %>% unlist
+df2 <- df[idx,] %>% select(state, racha, inicio, fin)
+write.csv(df2, file=stdout())
+close(f)
+
