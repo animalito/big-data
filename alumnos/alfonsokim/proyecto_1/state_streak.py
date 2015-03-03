6fa65621e941e0be4f0e32a1427bc39568c04877
Alfonso Kim <alfonsokim84@gmail.com>
2015-03-02 23:34:48 -0600

    documentacion del python

--- alumnos/alfonsokim/proyecto_1/state_streak.py
+++ alumnos/alfonsokim/proyecto_1/state_streak.py
@@ -4,7 +4,9 @@ from datetime import datetime, date
 
 # ==================================================================
 def safe_parse_date(date_str):
-    """
+    """ Intenta convertir un string a fecha usando 2 formatos,
+        si no puede devuelve None en lugar de tronar
+        :param date_str: La fecha en string
     """
     try:
         return datetime.strptime(date_str, '%m/%d/%y %H:%M')
@@ -17,7 +19,8 @@ def safe_parse_date(date_str):
 
 # ==================================================================
 def build_streaks(dates):
-    """
+    """ Encuentra las rachas en una lista de fechas
+        :param dates: La lista de fechas
     """
     dates.sort()
     streaks = []
@@ -25,10 +28,10 @@ def build_streaks(dates):
     end = dates[0]
     for current in dates[1:]:
         delta = current - end
-        if delta.days == 1:
+        if delta.days == 1: # Sigue la racha
             end = current
-        else:
-            if (end - begin).days > 1:
+        else:       # Fin de la racha
+            if (end - begin).days > 1: # Es racha solo si la dif de dias es mayor a 1
                 streaks.append((begin, end, (end - begin).days))
             begin = current
             end = begin
@@ -37,7 +40,10 @@ def build_streaks(dates):
 
 # ==================================================================
 def parse_streak(state, header=True):
-    """
+    """ Procesa las rachas de un estado, o de todos si no se
+        especifica ninguno
+        :param state: El estado a encontrar, o None si se buscan todos
+        :param header: Si los datos tienen encabezado
     """
     #print 'Max streaks for state %s' % (state or 'all')
     if header: sys.stdin.readline() 
@@ -58,6 +64,7 @@ def parse_streak(state, header=True):
 
 # ==================================================================
 if __name__ == '__main__':
-    """ Punto de entrada a la consola
+    """ Punto de entrada a la consola. Puede recibir de argumento 
+        el codigo de pais al que se busca la racha
     """
     parse_streak(sys.argv[1] if len(sys.argv) > 1 else False)
