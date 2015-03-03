94c193d1cf64372b5c69fb5a3b53a3e7936e375a
animalito <animalsalvaje@gmail.com>
2015-03-02 18:43:13 -0600

    ya funciona el transform

--- alumnos/animalito/proyecto_bash/src/4_transform.sh
+++ alumnos/animalito/proyecto_bash/src/4_transform.sh
@@ -9,4 +9,8 @@ echo Ejecutando $SOURCE
 echo Carpeta de ejecución $DIR
 
 echo Pasamos de html a csv
-parallel --basefile transform_html.r --slf $DIR/../data/instancias.txt "ls ~/ | grep -E '*.html$' | xargs -0 -d'\n' -I {} ./transform_htmls.r {} $(basename {}).csv"
+#parallel --basefile transform_html.r --slf $DIR/../data/instancias.txt "ls ~/ | grep -E '*.html$' | xargs -0 -d'\n' -I {} ./transform_htmls.r {} $(basename {}).csv"
+
+#parallel  --nonall --basefile transform_htmls.r --slf ../data/instancias.txt "find . *.html -exec ./transform_htmls.r \{\} \{\}.csv \;"
+
+parallel  --nonall --basefile transform_htmls.r --slf ../data/instancias.txt "find . *.html -exec ./transform_htmls.r \{\} \{.\}.csv \;"
